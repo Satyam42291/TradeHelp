94f121fe6f326b212ae269e0de9a82fd38427b83
Satyam Kumar <97077852+Satyam42291@users.noreply.github.com>
2024-07-08 18:22:04 +0530

    app.py

--- TradeHelp_Trading_made_easy/app.py
+++ TradeHelp_Trading_made_easy/app.py
@@ -111,56 +111,8 @@ fig1.update_layout(
 )
 st.plotly_chart(fig1)
 
-# fig1 = plot_plotly(m, forecast)
-# st.plotly_chart(fig1)
-
 st.write("Forecast components")
 fig2 = m.plot_components(forecast)
 st.write(fig2)
 
-
-
-
-
-
-
-
-
-
-# data_training=pd.DataFrame(df['Close'][0:int(len(df)*0.70)])
-# data_testing=pd.DataFrame(df['Close'][int(len(df)*0.70):int(len(df))])
-
-# from sklearn.preprocessing import MinMaxScaler
-# scaler=MinMaxScaler(feature_range=(0,1))
-# data_training_array=scaler.fit_transform(data_training)
-
-# model=load_model('keras_model.h5')
-
-# past_100_days=data_training.tail(100)
-# final_df= past_100_days.append(data_testing, ignore_index=True)
-# input_data=scaler.fit_transform(final_df)
-
-# x_test=[]
-# y_test=[]
-# for i in range(100,input_data.shape[0]):
-#     x_test.append(input_data[i-100:i])
-#     y_test.append(input_data[i,0])
-
-# x_test, y_test= np.array(x_test), np.array(y_test)
-
-# y_predicted=model.predict(x_test)
-
-# scaler=scaler.scale_
-# scale_factor=1/scaler[0]
-# y_predicted =y_predicted*scale_factor
-# y_test=y_test*scale_factor
-
-# st.subheader('Predicted vs Actual')
-# fig2=plt.figure(figsize=(12,6))
-# plt.plot(y_test,'b', label='Original Price')
-# plt.plot(y_predicted,'r', label='Predicted Price')
-# plt.xlabel('Time')
-# plt.ylabel('Price')
-# plt.legend()
-# st.pyplot(fig2)
 
