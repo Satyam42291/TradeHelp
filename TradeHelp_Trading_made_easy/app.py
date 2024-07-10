a0fab223b4021ee2c70b53c7e7b16e504591dda0
Satyam Kumar <97077852+Satyam42291@users.noreply.github.com>
2024-07-10 16:48:48 +0530

    app.py

--- TradeHelp_Trading_made_easy/app.py
+++ TradeHelp_Trading_made_easy/app.py
@@ -1,13 +1,14 @@
-import streamlit as st
-from datetime import date
-
 import yfinance as yf
 from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
+import streamlit as st
+from datetime import date
+
 
 start = '2000-01-01'
 today = date.today().strftime("%Y-%m-%d")
+# today = '2022-12-31'
 
 st.title('TradeHelp - Trading made easy ;)')
 
@@ -33,19 +34,24 @@ def plot_raw_data():
 	fig = go.Figure()
 	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
 	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
-	fig.layout.update(title_text='Time Series data', xaxis_rangeslider_visible=True)
+	fig.layout.update(title_text='Time Series data',
+                    xaxis_rangeslider_visible=True,
+                    xaxis_title='Time',
+                    yaxis_title='Price'
+                    )
 	st.plotly_chart(fig)
 
 plot_raw_data()
 
-st.subheader('Closing Price vs Time chart with 100MA and 200MA')
+# st.subheader('Closing Price vs Time chart with 100MA and 200MA')
 ma100 = data.Close.rolling(100).mean()
 ma200 = data.Close.rolling(200).mean()
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=data.index, y=ma100, mode='lines', name='100MA', line=dict(color='red')))
-fig.add_trace(go.Scatter(x=data.index, y=ma200, mode='lines', name='200MA', line=dict(color='green')))
-fig.add_trace(go.Scatter(x=data.index, y=data.Close, mode='lines', name='Closing Price', line=dict(color='blue')))
+fig.add_trace(go.Scatter(x=data['Date'], y=ma100, mode='lines', name='100MA', line=dict(color='red')))
+fig.add_trace(go.Scatter(x=data['Date'], y=ma200, mode='lines', name='200MA', line=dict(color='green')))
+fig.add_trace(go.Scatter(x=data['Date'], y=data.Close, mode='lines', name='Closing Price', line=dict(color='blue')))
 fig.update_layout(
+    xaxis_rangeslider_visible=True,
     width=800, 
     height=600,
     title='Closing Price vs Time chart with 100MA and 200MA',
@@ -103,6 +109,7 @@ fig1.add_trace(go.Scatter(
 ))
 
 fig1.update_layout(
+    xaxis_rangeslider_visible=True,
     width=800, 
     height=600,
     title=f'Forecast plot for {n_years} years',
@@ -116,3 +123,4 @@ fig2 = m.plot_components(forecast)
 st.write(fig2)
 
 
+# FB PROPHET, RMSE=0.935556, calculation_time=0.659962893 approx.
